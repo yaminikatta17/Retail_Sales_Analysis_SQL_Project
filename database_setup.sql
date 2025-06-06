3d827b063557e5e28c57911c3330bc58e50428ef
yaminikatta17 <yaminikatta17@gmail.com>
2025-06-07 00:56:47 +0530

    Add files via upload

---
+++ database_setup.sql
@@ -0,0 +1,30 @@
+-- SQL Retail Sales Analysis - P1
+CREATE DATABASE sql_project_p2;
+
+
+-- Create TABLE
+DROP TABLE IF EXISTS retail_sales;
+CREATE TABLE retail_sales
+            (
+                transaction_id INT PRIMARY KEY,	
+                sale_date DATE,	 
+                sale_time TIME,	
+                customer_id	INT,
+                gender	VARCHAR(15),
+                age	INT,
+                category VARCHAR(15),	
+                quantity	INT,
+                price_per_unit FLOAT,	
+                cogs	FLOAT,
+                total_sale FLOAT
+            );
+
+SELECT * FROM retail_sales
+LIMIT 10;
+
+
+    
+
+SELECT 
+    COUNT(*) 
+FROM retail_sales;
